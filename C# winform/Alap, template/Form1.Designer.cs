bad3775a8f6e5ed09221e6e9b404129793f1f85a
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-10-18 17:54:15 +0200

    C# winform created, menu started
    did the labeling for the menu(s), started writing methods.

---
+++ C# winform/Alap, template/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Alap__template
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eszközökFelméréseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eszközökInicializálásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilépésAProgrambólToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tesztekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.csoportküldésTeszteléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programKészítéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ütemHozzáadásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programokKezeléseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feladatsorBetöltéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feladatsorMentéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feladatsorSzerkesztéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feladatsorFuttatásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem,
+            this.tesztekToolStripMenuItem,
+            this.programKészítéseToolStripMenuItem,
+            this.programokKezeléseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eszközökFelméréseToolStripMenuItem,
+            this.eszközökInicializálásaToolStripMenuItem,
+            this.kilépésAProgrambólToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // eszközökFelméréseToolStripMenuItem
+            // 
+            this.eszközökFelméréseToolStripMenuItem.Name = "eszközökFelméréseToolStripMenuItem";
+            this.eszközökFelméréseToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.eszközökFelméréseToolStripMenuItem.Text = "Eszközök felmérése";
+            this.eszközökFelméréseToolStripMenuItem.Click += new System.EventHandler(this.eszközökFelméréseToolStripMenuItem_Click);
+            // 
+            // eszközökInicializálásaToolStripMenuItem
+            // 
+            this.eszközökInicializálásaToolStripMenuItem.Name = "eszközökInicializálásaToolStripMenuItem";
+            this.eszközökInicializálásaToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.eszközökInicializálásaToolStripMenuItem.Text = "Eszközök inicializálása";
+            // 
+            // kilépésAProgrambólToolStripMenuItem
+            // 
+            this.kilépésAProgrambólToolStripMenuItem.Name = "kilépésAProgrambólToolStripMenuItem";
+            this.kilépésAProgrambólToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.kilépésAProgrambólToolStripMenuItem.Text = "Kilépés a programból";
+            this.kilépésAProgrambólToolStripMenuItem.Click += new System.EventHandler(this.kilépésAProgrambólToolStripMenuItem_Click);
+            // 
+            // tesztekToolStripMenuItem
+            // 
+            this.tesztekToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.csoportküldésTeszteléseToolStripMenuItem});
+            this.tesztekToolStripMenuItem.Name = "tesztekToolStripMenuItem";
+            this.tesztekToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.tesztekToolStripMenuItem.Text = "Tesztek";
+            // 
+            // csoportküldésTeszteléseToolStripMenuItem
+            // 
+            this.csoportküldésTeszteléseToolStripMenuItem.Name = "csoportküldésTeszteléseToolStripMenuItem";
+            this.csoportküldésTeszteléseToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.csoportküldésTeszteléseToolStripMenuItem.Text = "Csoportküldés tesztelése";
+            // 
+            // programKészítéseToolStripMenuItem
+            // 
+            this.programKészítéseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ütemHozzáadásaToolStripMenuItem});
+            this.programKészítéseToolStripMenuItem.Name = "programKészítéseToolStripMenuItem";
+            this.programKészítéseToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
+            this.programKészítéseToolStripMenuItem.Text = "Program készítése";
+            // 
+            // ütemHozzáadásaToolStripMenuItem
+            // 
+            this.ütemHozzáadásaToolStripMenuItem.Name = "ütemHozzáadásaToolStripMenuItem";
+            this.ütemHozzáadásaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ütemHozzáadásaToolStripMenuItem.Text = "Ütem hozzáadása";
+            // 
+            // programokKezeléseToolStripMenuItem
+            // 
+            this.programokKezeléseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.feladatsorBetöltéseToolStripMenuItem,
+            this.feladatsorMentéseToolStripMenuItem,
+            this.feladatsorSzerkesztéseToolStripMenuItem,
+            this.feladatsorFuttatásaToolStripMenuItem});
+            this.programokKezeléseToolStripMenuItem.Name = "programokKezeléseToolStripMenuItem";
+            this.programokKezeléseToolStripMenuItem.Size = new System.Drawing.Size(124, 20);
+            this.programokKezeléseToolStripMenuItem.Text = "Programok kezelése";
+            // 
+            // feladatsorBetöltéseToolStripMenuItem
+            // 
+            this.feladatsorBetöltéseToolStripMenuItem.Name = "feladatsorBetöltéseToolStripMenuItem";
+            this.feladatsorBetöltéseToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.feladatsorBetöltéseToolStripMenuItem.Text = "Feladatsor betöltése";
+            // 
+            // feladatsorMentéseToolStripMenuItem
+            // 
+            this.feladatsorMentéseToolStripMenuItem.Name = "feladatsorMentéseToolStripMenuItem";
+            this.feladatsorMentéseToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.feladatsorMentéseToolStripMenuItem.Text = "Feladatsor mentése";
+            // 
+            // feladatsorSzerkesztéseToolStripMenuItem
+            // 
+            this.feladatsorSzerkesztéseToolStripMenuItem.Name = "feladatsorSzerkesztéseToolStripMenuItem";
+            this.feladatsorSzerkesztéseToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.feladatsorSzerkesztéseToolStripMenuItem.Text = "Feladatsor szerkesztése";
+            // 
+            // feladatsorFuttatásaToolStripMenuItem
+            // 
+            this.feladatsorFuttatásaToolStripMenuItem.Name = "feladatsorFuttatásaToolStripMenuItem";
+            this.feladatsorFuttatásaToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.feladatsorFuttatásaToolStripMenuItem.Text = "Feladatsor futtatása";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eszközökFelméréseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eszközökInicializálásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kilépésAProgrambólToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tesztekToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem csoportküldésTeszteléseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programKészítéseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ütemHozzáadásaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programokKezeléseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feladatsorBetöltéseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feladatsorMentéseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feladatsorSzerkesztéseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feladatsorFuttatásaToolStripMenuItem;
+    }
+}
+
---
+++ C# winform/Alap, template/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alap__template
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kilépésAProgrambólToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void eszközökFelméréseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //start(); //ezt a dll-ből kapom
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Eszköz inicializálási hiba, ellenőrizze az eszközök csatlakozóit! ");
+            }
+            
+        }
+    }
+}
