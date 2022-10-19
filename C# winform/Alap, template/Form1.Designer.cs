3066726def658b050f5c54391a8c5f754f9f47ae
Sipos Levente <91262000+D985ET@users.noreply.github.com>
2022-10-19 12:06:59 +0200

    Winform template quite done
    Did the panel, which i copyied from the Project1.dps file from the somodiproject

--- C# winform/Alap, template/Form1.Designer.cs
+++ C# winform/Alap, template/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Alap__template
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.eszközökFelméréseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -43,7 +44,20 @@ namespace Alap__template
             this.feladatsorMentéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.feladatsorSzerkesztéseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.feladatsorFuttatásaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.BTN_save = new System.Windows.Forms.Button();
+            this.BTNopen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Panel2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -55,7 +69,7 @@ namespace Alap__template
             this.programokKezeléseToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(854, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -81,6 +95,7 @@ namespace Alap__template
             this.eszközökInicializálásaToolStripMenuItem.Name = "eszközökInicializálásaToolStripMenuItem";
             this.eszközökInicializálásaToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
             this.eszközökInicializálásaToolStripMenuItem.Text = "Eszközök inicializálása";
+            this.eszközökInicializálásaToolStripMenuItem.Click += new System.EventHandler(this.eszközökInicializálásaToolStripMenuItem_Click);
             // 
             // kilépésAProgrambólToolStripMenuItem
             // 
@@ -102,6 +117,7 @@ namespace Alap__template
             this.csoportküldésTeszteléseToolStripMenuItem.Name = "csoportküldésTeszteléseToolStripMenuItem";
             this.csoportküldésTeszteléseToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
             this.csoportküldésTeszteléseToolStripMenuItem.Text = "Csoportküldés tesztelése";
+            this.csoportküldésTeszteléseToolStripMenuItem.Click += new System.EventHandler(this.csoportküldésTeszteléseToolStripMenuItem_Click);
             // 
             // programKészítéseToolStripMenuItem
             // 
@@ -114,8 +130,9 @@ namespace Alap__template
             // ütemHozzáadásaToolStripMenuItem
             // 
             this.ütemHozzáadásaToolStripMenuItem.Name = "ütemHozzáadásaToolStripMenuItem";
-            this.ütemHozzáadásaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ütemHozzáadásaToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
             this.ütemHozzáadásaToolStripMenuItem.Text = "Ütem hozzáadása";
+            this.ütemHozzáadásaToolStripMenuItem.Click += new System.EventHandler(this.ütemHozzáadásaToolStripMenuItem_Click);
             // 
             // programokKezeléseToolStripMenuItem
             // 
@@ -152,17 +169,115 @@ namespace Alap__template
             this.feladatsorFuttatásaToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
             this.feladatsorFuttatásaToolStripMenuItem.Text = "Feladatsor futtatása";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Információs sáv";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Rich Text Format (.rtf)|*.rtf|Png Image (.png)|*.png|C#files|*.cs|All files|*.*";
+            // 
+            // BTN_save
+            // 
+            this.BTN_save.Location = new System.Drawing.Point(97, 37);
+            this.BTN_save.Name = "BTN_save";
+            this.BTN_save.Size = new System.Drawing.Size(43, 23);
+            this.BTN_save.TabIndex = 3;
+            this.BTN_save.Text = "&Save";
+            this.BTN_save.UseVisualStyleBackColor = true;
+            this.BTN_save.Click += new System.EventHandler(this.BTN_save_Click);
+            // 
+            // BTNopen
+            // 
+            this.BTNopen.Location = new System.Drawing.Point(146, 37);
+            this.BTNopen.Name = "BTNopen";
+            this.BTNopen.Size = new System.Drawing.Size(43, 23);
+            this.BTNopen.TabIndex = 4;
+            this.BTNopen.Text = "&Open";
+            this.BTNopen.UseVisualStyleBackColor = true;
+            this.BTNopen.Click += new System.EventHandler(this.BTNopen_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Rich Text Format (.rtf)|*.rtf|Png Image (.png)|*.png|C#files|*.cs|All files|*.*";
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(392, 37);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(414, 45);
+            this.trackBar.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 66);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(830, 412);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(822, 386);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ütemek beállítása";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(822, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Programszöveg";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Panel2
+            // 
+            this.Panel2.Location = new System.Drawing.Point(311, 37);
+            this.Panel2.Name = "Panel2";
+            this.Panel2.Size = new System.Drawing.Size(75, 23);
+            this.Panel2.TabIndex = 8;
+            this.Panel2.Text = "&mainPanel";
+            this.Panel2.UseVisualStyleBackColor = true;
+            this.Panel2.Click += new System.EventHandler(this.Panel2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(854, 490);
+            this.Controls.Add(this.Panel2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.BTNopen);
+            this.Controls.Add(this.BTN_save);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,6 +299,17 @@ namespace Alap__template
         private System.Windows.Forms.ToolStripMenuItem feladatsorMentéseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem feladatsorSzerkesztéseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem feladatsorFuttatásaToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button BTN_save;
+        private System.Windows.Forms.Button BTNopen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button Panel2;
     }
 }
 
--- C# winform/Alap, template/Form1.cs
+++ C# winform/Alap, template/Form1.cs
@@ -34,5 +34,134 @@ namespace Alap__template
             }
             
         }
+
+        private void eszközökInicializálásaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int r;
+            //--------------------------TODO--------------------------
+            //r = SLDLL_felemeres(); //TODO
+            //label1.Caption = r.ToString(); //TODO (label1.Caption := inttostr(r);)
+        }
+
+        private void csoportküldésTeszteléseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i;
+            int elemszam;
+            //--------------------------TODO--------------------------
+            /*
+             * lista[0].azonos := dev485[0].azonos;
+               lista[0].vilrgb.rossze := 0;
+               lista[0].vilrgb.gossze := 0;
+               lista[0].vilrgb.bossze := 0;
+               devList[i].nilmeg := 0; // 1 //2 strToBool(elements[3]);
+
+               lista[1].azonos := dev485[1].azonos;
+               lista[1].vilrgb.rossze := 0;
+               lista[1].vilrgb.gossze := 0;
+               lista[1].vilrgb.bossze := 0;
+               devList[i].nilmeg := 0; // 1 //2 strToBool(elements[3]);
+
+               lista[2].azonos := dev485[2].azonos;
+               devList[i].nilmeg := 0; // 1 //2 strToBool(elements[3]);
+               lista[2].vilrgb.rossze := 20;
+               lista[2].vilrgb.gossze := 0;
+               lista[2].vilrgb.bossze := 0;
+
+
+               lista[3].azonos := dev485[3].azonos;
+               lista[3].vilrgb.rossze := 20;
+               lista[3].vilrgb.gossze := 0;
+               lista[3].vilrgb.bossze := 0;
+               devList[i].nilmeg := 0; // 1 //2 strToBool(elements[3]);
+
+               elemszam := 2;
+
+               i := SLDLL_SetLista(elemszam, lista);
+            */
+        }
+
+        private void ujlista()
+        {
+            //--------------------------TODO--------------------------
+            //inc(LISTADB); //TODO
+            //eszkozPanelLista(ScrollBox1); //TODO
+        }
+        private void ütemHozzáadásaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ujlista();
+        }
+
+        private void BTN_save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //--------------------------TODO--------------------------
+                //richTextBox.SaveFile(saveFileDialog.FileName);
+            }
+        }
+
+        private void BTNopen_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                //--------------------------TODO--------------------------
+                //richTextBox.LoadFile(openFileDialog.FileName);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //--------------------------TODO--------------------------
+            /*
+            Label1.Caption := inttostr(MyCounter + 1) + '. kör';
+            setRound(myCounter);
+            //label1.Caption := teststring;
+            if( MyCounter < SLprogram.Count - 1 )
+            {
+                inc(MyCounter);
+            }
+            else
+            {
+               Timer1.Enabled := false;
+            }
+            */
+        }
+
+        private void Panel2_Click(object sender, EventArgs e) 
+        {
+            string N, N7;
+            //------------------TODO--------------
+            //deviceType DEV;
+            List<int> container = new List<int>();
+            //------------------TODO--------------
+            /*
+             * AKT_PANEL:= Sender as TPanel;
+                DEVICE_select_by_button(Sender);
+                AKT_MENTETT_ESZKOZ.active := NOT AKT_MENTETT_ESZKOZ.active;
+                save_selectted_DEVICE();
+            */
+            /*
+            if ((Sender as TPanel).BevelOuter = bvRaised)
+            {
+
+                (Sender as TPanel).BevelOuter := bvLowered;
+                //    (Sender as TPanel).Color := clRed;
+                //    (Sender as TPanel).Font.Color := clWhite;
+                (Sender as TPanel).PopupMenu := PopupMenu1;
+                AKT_PANEl.BevelWidth := 1;
+            }
+            else
+            {
+                //(Sender as TPanel).Color := clBtnFace;
+                (Sender as TPanel).BevelOuter := bvRaised;
+                //(Sender as TPanel).Font.Color := clBlack;
+                (Sender as TPanel).PopupMenu := nil;
+                AKT_PANEl.BevelWidth := 1;
+            }
+            */
+
+
+        }
+
     }
 }
